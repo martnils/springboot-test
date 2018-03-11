3353b4ccd2d7d31baadeaf50c54fcea0b8ae21e3
MN <c.martin.nilsson@gmail.com>
2018-03-11 18:25:08 +0100

    2 lines

--- main.java
+++ main.java
@@ -3,4 +3,6 @@ public static void main(String [] args)
             System.out.println("1");
             System.out.println("2");
             System.out.println("3");
+            System.out.println("4");
+            System.out.println("5");
         }
