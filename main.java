35791b824c367415948dd1ba03ed2cb8d4d8c303
MN <c.martin.nilsson@gmail.com>
2018-08-06 20:19:42 +0200

    Rad4changed and Rad9 New

--- main.java
+++ main.java
@@ -1,9 +1,10 @@
 public static void main(String [] args)
         {
             System.out.println("1");
-            System.out.println("2");
+            System.out.println("Changed in featurebranch");
             System.out.println("3");
             System.out.println("4");
             System.out.println("5");
             System.out.println("Rad8 branch2");
+            System.out.println("Rad9 featurebranch");
         }
