0339faac02af7ce168bca7414d5cee2e44f12cd6
MN <c.martin.nilsson@gmail.com>
2018-08-06 19:44:32 +0200

    Rad8 branch2

--- main.java
+++ main.java
@@ -5,4 +5,5 @@ public static void main(String [] args)
             System.out.println("3");
             System.out.println("4");
             System.out.println("5");
+            System.out.println("Rad8 branch2");
         }
